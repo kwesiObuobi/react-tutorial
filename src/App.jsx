7e6f18487a046e43b76da9e8e6d23a76158ec0be
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-01 08:59:35 +0000

    Add Header component content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Header from '@/components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header />
     </>
   )
 }
