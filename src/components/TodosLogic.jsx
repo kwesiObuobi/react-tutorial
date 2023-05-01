be539f65ea9a50265213cfef0aaea2501458bea0
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-01 15:01:56 +0000

    Add icon for submit button

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,26 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import InputTodo from "./InputTodo";
 import TodosList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid';
 
 const TodosLogic = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: true,
-    },
-    {
-      id: uuidv4(),
-      title: 'Develop website and add content',
-      completed: false,
-    },
-    {
-      id: uuidv4(),
-      title: 'Deploy to live server',
-      completed: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(getInitialTodos());
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
+
+  function getInitialTodos() {
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
 
   const addTodoItem = (title) => {
     const newTodo = {
