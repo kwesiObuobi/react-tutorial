1d4f642860d80b045d339c77013ec04e7beb36e3
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-01 13:55:47 +0000

    Add View mode and Edit mode to TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from '@/styles/TodoItem.module.css';
 
 const TodoItem = ({ itemProp, setTodos, delTodo }) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  }
+
   const handleChange = (id) => {
     setTodos((prevState) =>
       prevState.map((todo) => {
@@ -16,18 +23,33 @@ const TodoItem = ({ itemProp, setTodos, delTodo }) => {
     );
   }
 
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
   return (
   <li className={styles.item}>
-    <div className={styles.content}>
+    <div className={styles.content} style={viewMode}>
       <input
         type="checkbox"
         checked={itemProp.completed}
         onChange={() => handleChange(itemProp.id)}
       />
-      <button>Edit</button>
+      <button onClick={ handleEditing }>Edit</button>
       <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       {itemProp.title}
     </div>
+    <input
+      type="text"
+      value={itemProp.title}
+      className={styles.textInput}
+      style={editMode}
+      onChange={(e) => console.log(e.target.value, itemProp.id)}
+    />
   </li>
   );
 };
